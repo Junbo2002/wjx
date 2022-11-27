c8e4b4a5faa2dc08515ce72990ae08d30da727a6
guohu7777 <1903642057@qq.com>
2022-11-27 13:25:44 +0800

    modified:   main.py

--- main.py
+++ main.py
@@ -69,10 +69,10 @@ def random_position():
 
 chrome_options = Options()
 # 设置无头浏览器
-# chrome_options.add_argument('--no-sandbox')
-# chrome_options.add_argument('--disable-dev-shm-usage')
-# chrome_options.add_argument('--headless')
-# chrome_options.add_argument('--disable-gpu')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--disable-gpu')
 
 # 滑块防止检测
 chrome_options.add_argument("--incognito") 
